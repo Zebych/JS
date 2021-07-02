1b9694000b1e7c8724f2e68e5f2372888ec32278
Zebych <81613417+Zebych@users.noreply.github.com>
2021-07-02 22:30:51 +0300

    exam native JS

--- scopes.js
+++ scopes.js
@@ -232,7 +232,179 @@ function checkAlive (health) {
 }*/
 
 //Вы получаете массив чисел, возвращаете сумму всех положительных чисел.
+/*
 function positiveSum(arr) {
  return arr.reduce((a,b)=>b>0?a+b:a,0)
 }
-positiveSum([-1,2,3,4,-5])
+positiveSum([-1,2,3,4,-5])*/
+
+
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 90
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    },
+    {
+        name: "Helen",
+        age: 20,
+        isMarried: false,
+        scores: 110
+    },
+    {
+        name: "Ann",
+        age: 20,
+        isMarried: false,
+        scores: 105
+    },
+];
+
+const user = {
+    name: "Bob",
+    age: 23,
+    friends: ["Alex", "Nick", "John"]
+}
+
+//1. Создайте поверхностную копию объекта user
+let copyUser = {...user};
+copyUser.friends[0] = 'Bob'
+
+//Проверка:
+console.log(user === copyUser)//false
+console.log(user.friends === copyUser.friends)//true
+
+//2. Полная (глубокая) копия объекта user
+let deepCopyUser = {...user, friends: [...user.friends]};
+deepCopyUser.friends[0] = 'Alexandr'
+
+//Проверка:
+console.log(user === deepCopyUser) //false
+console.log(user.friends === deepCopyUser.friends) //false
+
+//3. Поверхностная копия массива students
+let copyStudents = [...students];
+copyStudents[0].name = 'Alexandr'
+console.log(copyStudents)
+console.log(students)
+//Проверка:
+console.log(students === copyStudents)//false
+console.log(students[0].name === copyStudents[0].name)//true
+
+//4*. Полная (глубокая) копия массива students (map)
+let deepCopyStudents = students.map(st => ({...st}));
+deepCopyStudents[0].age = 32
+console.log(students)
+console.log(deepCopyStudents)
+//Проверка:
+console.log(students === deepCopyStudents) //false
+console.log(students[0].age === deepCopyStudents[0].age) //false
+
+// NB!!! Далее все преобразования выполняем не модифицируя исходный массив students
+// Вывод результатов - в консоль
+
+//5. Отсортируйте deepCopyStudents по алфавиту (sort)
+let sortByName = deepCopyStudents.map(d => d.name).sort();
+console.log(sortByName);
+
+//5a. Отсортируйте deepCopyStudents по успеваемости(лучший идёт первым)(sort)
+let sortByScores = deepCopyStudents.sort((a, b) => a.scores - b.scores).reverse();
+console.log(sortByScores);
+
+//6. Сформируйте массив студентов, у которых 100 и более баллов (filter)
+let bestStudents = students.filter((s) => s.scores >= 100);
+console.log(bestStudents)
+
+//6a. Получите массив ("вырежьте") из трёх лучших студентов из массива deepCopyStudents (splice)
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+let topStudents = deepCopyStudents.splice(0, 3);
+console.log(topStudents)
+console.log(deepCopyStudents)
+
+//6b. Объедините массивы deepCopyStudents и topStudents так,
+// чтоб сохранился порядок сортировки (spread-оператор )
+let newDeepCopyStudents = [...topStudents, ...deepCopyStudents];
+console.log(newDeepCopyStudents)
+
+//7. Сформируйте массив холостых студентов (filter)
+let notMarriedStudents = newDeepCopyStudents.filter(n => !n.isMarried);
+console.log(notMarriedStudents)
+
+//8. Сформируйте массив имён студентов (map)
+let studentsNames = newDeepCopyStudents.map(n => n.name);
+console.log(studentsNames)
+
+//8a. Сформируйте строку из имён студентов, разделённых
+// - запятой (join)
+// - пробелом (join)
+let nameWithSpace = studentsNames.join(',');
+console.log(nameWithSpace)
+let namesWithComma = studentsNames.join(' ');
+console.log(namesWithComma)
+
+//9. Добавьте всем студентам свойство "isStudent" со значением true (map)
+let trueStudents = students.map(s => ({...s, isStudent: true}));
+console.log(trueStudents)
+
+//10. Nick женился. Выполните выполните соответствующие преобразование массива students (map)
+let studentsWithMarriedNick = students.map(n => n.name === 'Nick' ? {...n, isMarried: true} : n)
+
+console.log(studentsWithMarriedNick)
+
+//11. Найдите студента по имени Ann (find)
+let ann = students.find(s => s.name = "Ann");
+console.log(ann)
+
+//12. Найдите студента с самым высоким баллом (reduce)
+let bestStudent = students.reduce((acc, el) => acc.scores > el.scores ? acc : el, 0);
+console.log(bestStudent)
+
+//13. Найдите сумму баллов всех студентов (reduce)
+let scoresSum = students.reduce((acc, el) => acc + el.scores, 0);
+console.log(scoresSum)
+
+// 14.Напишите функцию addFriends, которая принимает параметром массив students
+// и добавляет в каждому студенту свойство "friends",
+// значением которого является массив имён всех остальных студентов из массива,
+// за исключением собственного имени студента. Т.е. в друзьях у Боба Боба быть не должно.
+
+//не верно решено
+const addFriends = (students) => {
+        return students.map(s => ({...s, friends: students.map(s => s.name).filter(st => st !== s.name)}))
+}
+console.log(addFriends(students));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
